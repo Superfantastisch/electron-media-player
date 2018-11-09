ebbe120952690e952c0dcabacbdb190ad4a87ee8
Unknown <arne.seidenfaden@konvoj.de>
2018-11-09 16:14:46 +0100

    header
    - adding material design
    - adding flex layout
    - adding header to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +18,10 @@ import { DetailPageComponent } from './detail-page/detail-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
