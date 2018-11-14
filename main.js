bbcba080ba33cf11ab29a1e6cfe4ecbdb8ae7f96
Duy Minh Vo <vminhd@gmail.com>
2018-11-14 21:54:46 +0100

    Add support for hot-reload in Electron.
    Use `npm run electron:serve` to start Electron with Angular's hot-reload.
    
    Requires packages 'wait-on' and 'electron-reload' so please do `npm install` before usage.

--- main.js
+++ main.js
@@ -1,43 +1,65 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const url = require("url");
-
-let win;
-// setup
-app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var win, serve;
+var args = process.argv.slice(1);
+serve = args.some(function (val) { return val === '--serve'; });
+electron_1.app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600 });
-
-  // load the dist folder from Angular
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-
-  // The following is optional and will open the DevTools:
-  // win.webContents.openDevTools()
-
-  win.on("closed", () => {
-    win = null;
-  });
+    var electronScreen = electron_1.screen;
+    var size = electronScreen.getPrimaryDisplay().workAreaSize;
+    // Create the browser window.
+    win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(__dirname + "/node_modules/electron")
+        });
+        win.loadURL('http://localhost:4200');
+    }
+    else {
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'dist/index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    }
+    // win.webContents.openDevTools();
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+        // Dereference the window object, usually you would store window
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
-
-app.on("ready", createWindow);
-
-// on macOS, closing the window doesn't quit the app
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-// initialize the app's main window
-app.on("activate", () => {
-  if (win === null) {
-    createWindow();
-  }
-});
+try {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    electron_1.app.on('ready', createWindow);
+    // Quit when all windows are closed.
+    electron_1.app.on('window-all-closed', function () {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            electron_1.app.quit();
+        }
+    });
+    electron_1.app.on('activate', function () {
+        // On OS X it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (win === null) {
+            createWindow();
+        }
+    });
+}
+catch (e) {
+    // Catch Error
+    // throw e;
+}
+//# sourceMappingURL=main.js.map
