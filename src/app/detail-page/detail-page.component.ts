097a71ce64ed27acc9d9334154fbf93e3c2013e5
Duy Minh Vo <vminhd@gmail.com>
2018-12-03 17:55:21 +0100

    Added player service so we have a single player instance through the application

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -4,6 +4,7 @@ import { MovieService } from '../movie.service';
 import { IMovie } from '../models/movies';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MATERIAL_SANITY_CHECKS_FACTORY } from '@angular/material/core/typings/common-behaviors/common-module';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-detail-page',
@@ -15,7 +16,7 @@ export class DetailPageComponent implements OnInit {
   movie: IMovie = null;
 
   constructor(
-    private movieService: MovieService,
+    private playerService: PlayerService,
     private router: Router,
     private route: ActivatedRoute
   ) {
@@ -25,29 +26,20 @@ export class DetailPageComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
-  setupComponent(): void {
-    // Install built-in polyfills to patch browser incompatibilities.
-    shaka.polyfill.installAll();
-
-    // Check to see if the browser supports the basic APIs Shaka needs.
-    if (shaka.Player.isBrowserSupported()) {
-      // Everything looks good!
-      this.initPlayer();
-    } else {
-      // This browser does not have the minimum set of APIs we need.
-      console.error('Browser not supported!');
-    }
-  }
-
   initPlayer(): void {
     // const video = getElementById('videoPlayer');
     const video = this.videoPlayer.nativeElement;
     console.log(video);
-    const player = new shaka.Player(video);
-    player.load(this.movie.manifestUri).then(function () {
-      // This runs if the asynchronous load is successful.
-      console.log('The video has now been loaded!');
-    }).catch((e) => {console.error(e)});
+    const player = this.playerService.player;
+
+    if (player) {
+      player.attach(video).then(_ => {
+        return player.load(this.movie.manifestUri);
+      }).then(_ => {
+         // This runs if the asynchronous load is successful.
+         console.log('The video has now been loaded!');
+      }).catch((e) => {console.error(e)});
+    }
   }
 
   ngOnInit() {
@@ -63,7 +55,7 @@ export class DetailPageComponent implements OnInit {
 
   ngAfterViewInit() {
     // after view init the video element/ shaka player should be initialized
-    this.setupComponent();
+    this.initPlayer();
   }
 
 }
