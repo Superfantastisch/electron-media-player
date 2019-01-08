9f5708f8e8e7abff102f86cd603a4ea9860c7f9e
Unknown <arne.seidenfaden@konvoj.de>
2019-01-08 19:18:58 +0100

    random background video

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { MOVIES, IMovie } from './models/movies';
 import { Observable, of, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { parseString } from 'xml2js';
 
 @Injectable({
   providedIn: 'root'
